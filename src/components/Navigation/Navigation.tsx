75e02b8212bd3f358abc6de890b49d60f1273319
Borza Peter <borzapeter@yahoo.com>
2021-11-09 14:49:34 +0200

    0010 - minor css on navbar fonts and button behaviour

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,19 +2,24 @@ import { useContext } from "react";
 import NavBar from "../../reusables/NavBar";
 import { NavLink, Outlet } from "react-router-dom";
 import { LinkContext } from "../../utils/link-context";
+import classNames from "classnames";
 
 import styles from "./Navigation.module.scss";
 
 const Navigation = () => {
 	const links = useContext(LinkContext);
 
+	const linkClasses = (isActive: boolean) =>
+		classNames(styles.links, {
+			[styles["active"]]: isActive,
+			[styles["inActive"]]: !isActive,
+		});
+
 	const renderBody = () => {
 		return links?.map(item => (
 			<li key={item.id}>
 				<NavLink
-					className={({ isActive }) =>
-						isActive ? styles.active : styles.inActive
-					}
+					className={({ isActive }) => linkClasses(isActive)}
 					to={item.to}
 				>
 					{item.label}
