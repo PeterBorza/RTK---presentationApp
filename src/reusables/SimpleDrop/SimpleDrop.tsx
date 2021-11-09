acd7806089ac017cc937487539c45c9c4daf80d5
Borza Peter <borzapeter@yahoo.com>
2021-11-09 10:49:08 +0200

    0009 - routes into App and css fixes to containers

--- src/reusables/SimpleDrop/SimpleDrop.tsx
+++ src/reusables/SimpleDrop/SimpleDrop.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useRef } from "react";
 import styles from "./SimpleDrop.module.scss";
 import classNames from "classnames";
 
