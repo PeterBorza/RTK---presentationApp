0423f0beb89bd29d83f4731885b6b3c5837f15a4
Borza Peter <borzapeter@yahoo.com>
2021-11-19 13:09:17 +0200

    0036 - moved hooks in src, App in app

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./app/App";
 import { Home } from "./components";
 import { Building } from "./components/building-story";
 import { GasExpenses } from "./components/gas-story";
