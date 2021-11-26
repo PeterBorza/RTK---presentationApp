e48af88dc7e9550dca1c7e399cad2aa1c2b2b156
Borza Peter <borzapeter@yahoo.com>
2021-11-26 18:05:25 +0200

    DEV_  reusable select component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,26 @@
-import { FC } from "react";
+import React, { FC } from "react";
 import { useTime } from "../../hooks";
-import { FlipBox, LoadingWrapper, Error as ErrorBlock } from "../../reusables";
+import {
+	FlipBox,
+	LoadingWrapper,
+	Error as ErrorBlock,
+	Select,
+} from "../../reusables";
 import { Error } from "../../app/constants";
 
 import styles from "./Home.module.scss";
 
 const Home: FC = () => {
+	const handler = (e: React.ChangeEvent<HTMLSelectElement>) =>
+		console.log(e.target.value);
+
+	const mySelect = {
+		options: ["small", "medium", "large", "xxl"],
+		name: "mySelect",
+		label: "Select a width",
+		// onChange: (e: React.ChangeEvent<HTMLSelectElement>) => handler(e),
+		onChange: handler,
+	};
 	const flipCard = [
 		{
 			frontContent: () => <LoadingWrapper loading={true} />,
@@ -32,12 +47,13 @@ const Home: FC = () => {
 
 	return (
 		<div className={styles.container}>
-			<div className={styles.flipWrap}>
+			{/* <div className={styles.flipWrap}>
 				<FlipBox {...flipCard[0]} />
 			</div>
 			<div className={styles.flipWrap}>
 				<FlipBox {...flipCard[1]} />
-			</div>
+			</div> */}
+			<Select {...mySelect} />
 		</div>
 	);
 };
