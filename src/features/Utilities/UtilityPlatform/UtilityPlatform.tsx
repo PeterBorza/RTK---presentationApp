13ff66e77645e119d189e42a6cec3acfe9554894
Borza Peter <borzapeter@yahoo.com>
2021-12-31 09:49:54 +0200

    0083 - game layout media query, custom input in utlilities platform

--- src/features/Utilities/UtilityPlatform/UtilityPlatform.tsx
+++ src/features/Utilities/UtilityPlatform/UtilityPlatform.tsx
@@ -1,11 +1,30 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { toggleUtils } from "../../../app";
+import { CustomInput } from "../../../shared-components";
 import styles from "./UtilityPlatform.module.scss";
 
 const UtilityPlatform: FC = () => {
+	const [name, setName] = useState("");
+	const validPass = name === "Peter";
+	const dispatch = useDispatch();
+	useEffect(() => {
+		validPass ? dispatch(toggleUtils(true)) : dispatch(toggleUtils(false));
+	}, [name]);
 	return (
 		<div className={styles.container}>
-			<h1>WELCOME TO MY UTILITIES APP</h1>
-			<p>needs work</p>
+			<h2 style={{ color: validPass ? "limegreen" : "crimson" }}>
+				{validPass
+					? "Correct password"
+					: "Type in owner's first name to see menu"}
+			</h2>
+			<div className={styles.inputContent}>
+				<CustomInput
+					name='Password'
+					value={name}
+					onChange={e => setName(e.target.value)}
+				/>
+			</div>
 		</div>
 	);
 };
