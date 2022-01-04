0e8ff18b11394894f1753ca0dc5f472d4a2eff3d
Borza Peter <peter.borza@anaplan.com>
2022-01-04 08:05:39 +0200

    0084 - minor issues

--- src/features/Utilities/UtilityPlatform/UtilityPlatform.tsx
+++ src/features/Utilities/UtilityPlatform/UtilityPlatform.tsx
@@ -5,28 +5,28 @@ import { CustomInput } from "../../../shared-components";
 import styles from "./UtilityPlatform.module.scss";
 
 const UtilityPlatform: FC = () => {
-	const [name, setName] = useState("");
-	const validPass = name === "Peter";
-	const dispatch = useDispatch();
-	useEffect(() => {
-		validPass ? dispatch(toggleUtils(true)) : dispatch(toggleUtils(false));
-	}, [name]);
-	return (
-		<div className={styles.container}>
-			<h2 style={{ color: validPass ? "limegreen" : "crimson" }}>
-				{validPass
-					? "Correct password"
-					: "Type in owner's first name to see menu"}
-			</h2>
-			<div className={styles.inputContent}>
-				<CustomInput
-					name='Password'
-					value={name}
-					onChange={e => setName(e.target.value)}
-				/>
-			</div>
-		</div>
-	);
+    const [name, setName] = useState("");
+    const validPass = name === "Peter";
+    const dispatch = useDispatch();
+    useEffect(() => {
+        validPass ? dispatch(toggleUtils(true)) : dispatch(toggleUtils(false));
+    }, [dispatch, validPass]);
+    return (
+        <div className={styles.container}>
+            <h2 style={{ color: validPass ? "limegreen" : "crimson" }}>
+                {validPass
+                    ? "Correct password"
+                    : "Type in owner's first name to see menu"}
+            </h2>
+            <div className={styles.inputContent}>
+                <CustomInput
+                    name="Password"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default UtilityPlatform;
--- src/features/memoryGame-story/memoryGameSlice.ts
+++ src/features/memoryGame-story/memoryGameSlice.ts
@@ -5,68 +5,68 @@ import { shuffledImages } from "./game-images";
 import { GamePhotoData } from ".";
 
 const initialState: MemoryGameState = {
-	photos: [...imageData()],
-	gamePhotos: [...shuffledImages],
-	pending: false,
-	error: false,
-	clickCount: 0,
+    photos: imageData(),
+    gamePhotos: shuffledImages,
+    pending: false,
+    error: false,
+    clickCount: 0,
 };
 
 export const memoryGameSlice = createSlice({
-	name: "memoryGame",
-	initialState,
-	reducers: {
-		setPending: (
-			{ pending }: MemoryGameState,
-			{ payload }: PayloadAction<boolean>
-		) => {
-			pending = payload;
-		},
-		setError: (
-			{ error }: MemoryGameState,
-			{ payload }: PayloadAction<boolean>
-		) => {
-			error = payload;
-		},
-		toggleFlip: (
-			{ gamePhotos }: MemoryGameState,
-			{ payload }: PayloadAction<string>
-		) => {
-			gamePhotos.map(
-				item => (item.isFlipped = item.id === payload ? true : false)
-			);
-		},
-		setMatch: (
-			{ gamePhotos }: MemoryGameState,
-			{ payload }: PayloadAction<{ id: string; match: boolean }>
-		) => {
-			const selected = gamePhotos.find(item => item.id === payload.id);
-			if (selected) selected.match = payload.match;
-		},
-		incrementCount: (state: MemoryGameState) => {
-			state.clickCount++;
-		},
-		resetGame: (state: MemoryGameState) => {
-			state.gamePhotos = initialState.gamePhotos;
-			state.clickCount = initialState.clickCount;
-		},
-		setNewGame: (
-			state: MemoryGameState,
-			{ payload }: PayloadAction<GamePhotoData[]>
-		) => {
-			state.gamePhotos = payload;
-		},
-	},
+    name: "memoryGame",
+    initialState,
+    reducers: {
+        setPending: (
+            { pending }: MemoryGameState,
+            { payload }: PayloadAction<boolean>
+        ) => {
+            pending = payload;
+        },
+        setError: (
+            { error }: MemoryGameState,
+            { payload }: PayloadAction<boolean>
+        ) => {
+            error = payload;
+        },
+        toggleFlip: (
+            { gamePhotos }: MemoryGameState,
+            { payload }: PayloadAction<string>
+        ) => {
+            gamePhotos.map(
+                item => (item.isFlipped = item.id === payload ? true : false)
+            );
+        },
+        setMatch: (
+            { gamePhotos }: MemoryGameState,
+            { payload }: PayloadAction<{ id: string; match: boolean }>
+        ) => {
+            const selected = gamePhotos.find(item => item.id === payload.id);
+            if (selected) selected.match = payload.match;
+        },
+        incrementCount: (state: MemoryGameState) => {
+            state.clickCount++;
+        },
+        resetGame: (state: MemoryGameState) => {
+            state.gamePhotos = initialState.gamePhotos;
+            state.clickCount = initialState.clickCount;
+        },
+        setNewGame: (
+            state: MemoryGameState,
+            { payload }: PayloadAction<GamePhotoData[]>
+        ) => {
+            state.gamePhotos = payload;
+        },
+    },
 });
 
 export const {
-	setPending,
-	setError,
-	toggleFlip,
-	setMatch,
-	incrementCount,
-	resetGame,
-	setNewGame,
+    setPending,
+    setError,
+    toggleFlip,
+    setMatch,
+    incrementCount,
+    resetGame,
+    setNewGame,
 } = memoryGameSlice.actions;
 
 export default memoryGameSlice.reducer;
