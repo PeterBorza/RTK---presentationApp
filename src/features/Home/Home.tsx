86d9c7c9b5e73a7a7292cd5b2124618e2f2f958c
Borza Peter <peter.borza@anaplan.com>
2022-03-01 19:00:56 +0200

    0103 - rubik into Home, Navigation responsive

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,27 +1,12 @@
 import React from "react";
-import { SimpleDrop } from "../../shared-components";
+import { Rubik } from "../../shared-components";
 
 import styles from "./Home.module.scss";
 
 const Home = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.dropHolder}>
-                <SimpleDrop height="medium">
-                    <ul>
-                        <li>item-1</li>
-                        <li>item-2</li>
-                        <li>item-3</li>
-                        <li>item-4</li>
-                        <li>item-5</li>
-                        <li>item-6</li>
-                        <li>item-7</li>
-                        <li>item-8</li>
-                        <li>item-9</li>
-                        <li>item-10</li>
-                    </ul>
-                </SimpleDrop>
-            </div>
+            <Rubik withAnimation />
         </div>
     );
 };
