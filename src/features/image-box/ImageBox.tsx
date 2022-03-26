c8a133bb8ade2779b68b604cad7f445a587c8a20
Borza Peter <borzapeter@yahoo.com>
2022-03-26 21:49:19 +0200

    0117 - ImageBox and puzzle images array added

--- src/features/image-box/ImageBox.tsx
+++ src/features/image-box/ImageBox.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { ImageCard } from "../../shared-components";
+import { folder } from "../../images/folder/image-stack";
 import styles from "./ImageBox.module.scss";
 
 const ImageBox = () => {
+    console.table(folder);
+
     return (
         <div className={styles["image-grid-container"]}>
-            <ImageCard />
+            {folder.map(img => (
+                <ImageCard
+                    position="top"
+                    src={img}
+                    caption=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt dolores enim iure! Culpa eaque nemo officia aspernatur debitis, atque commodi? Atque quae eos consectetur quas. Nisi voluptate ut ipsam quos! Cupiditate animi quaerat inventore saepe repudiandae distinctio ipsam. Magnam numquam dolorum nihil inventore fuga amet!"
+                />
+            ))}
         </div>
     );
 };
--- src/shared-components/ImageCard/ImageCard.tsx
+++ src/shared-components/ImageCard/ImageCard.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+
+import classNames from "classnames";
 import styles from "./ImageCard.module.scss";
 import ls11_900 from "../../images/ls11_900.jpg";
+import { ImageType } from "../../utils/my-images";
 
-const ImageCard = () => {
+type ImageCardType = {
+    position: "top" | "bottom";
+    src: ImageType;
+    caption: string;
+};
+
+const ImageCard = ({ position, src, caption }: ImageCardType) => {
+    const captionClasses = classNames(
+        styles["card__caption"],
+        styles[`card__caption__${position}`],
+    );
     return (
         <div className={styles["outer-wrapper"]}>
-            <img className={styles.card__image} src={ls11_900} alt="peisaj" />
+            <img className={styles.card__image} src={src} alt="peisaj" />
+            <p className={captionClasses}>{caption}</p>
         </div>
     );
 };
