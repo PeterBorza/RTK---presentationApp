7a7727c866881d35827352672fc8c8d18467e915
Borza Peter <peter.borza@anaplan.com>
2022-03-29 22:05:44 +0300

    0119 - scrollpage width adjust, image card click event

--- src/features/image-box/ImageBox.tsx
+++ src/features/image-box/ImageBox.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { v4 as uuid } from "uuid";
+
 import { ImageCard } from "../../shared-components";
 import { folder } from "../../images/folder/image-stack";
 
@@ -10,6 +12,7 @@ const ImageBox = () => {
         <div className={styles["image-grid-container"]}>
             {folder.map(img => (
                 <ImageCard
+                    key={uuid()}
                     position="top"
                     src={img}
                     caption=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt dolores enim iure! Culpa eaque nemo officia aspernatur debitis, atque commodi? Atque quae eos consectetur quas. Nisi voluptate ut ipsam quos! Cupiditate animi quaerat inventore saepe repudiandae distinctio ipsam. Magnam numquam dolorum nihil inventore fuga amet!"
