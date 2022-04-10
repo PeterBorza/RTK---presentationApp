66c8e734935fd7459c0ab139621ff15f6a94ec40
Peter Borza <68942716+PeterBorza@users.noreply.github.com>
2022-04-10 15:59:38 +0300

    0126 - rtk, sass and router npm update and app-routes extracted (#120)

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { store } from "./app/store";
-import App from "./app/App";
 
-import Home from "./features/Home";
-import { utilityRoutes } from "../src/features/Utilities";
-import { LinkUrls } from "./context/link-context";
-import ScrollPageContainer from "./features/scroll-pages";
-import { Photo, Photos } from "./features/memoryGame-story";
-import PhotosLandingPage from "./features/memoryGame-story/PhotosLandingPage";
 import { PagesContextProvider } from "./context/pages-context";
-
-const utilities = utilityRoutes();
+import AppRoutes from "./app-routes";
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
                 <PagesContextProvider>
-                    <Routes>
-                        <Route path={LinkUrls.HOME} element={<App />}>
-                            <Route index element={<Home />} />
-                            {utilities}
-                            <Route path={LinkUrls.PHOTOS} element={<Photos />}>
-                                <Route index element={<PhotosLandingPage />} />
-                                <Route path=":id" element={<Photo />} />
-                            </Route>
-                            <Route path={LinkUrls.SCROLL} element={<ScrollPageContainer />} />
-                        </Route>
-                    </Routes>
+                    <AppRoutes />
                 </PagesContextProvider>
             </Provider>
         </BrowserRouter>
