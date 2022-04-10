a1bc7e02c7db4c4dcf66944d269c69723d24e05c
Peter Borza <68942716+PeterBorza@users.noreply.github.com>
2022-04-10 17:07:39 +0300

    0129 - css reorganized, App.scss removed (#123)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,15 @@
 import Navigation from "../features/Navigation";
 import { LinkContextProvider } from "../context";
-import { Outlet } from "react-router";
-
-import styles from "./App.module.scss";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
     return (
-        <div className={styles.container}>
+        <>
             <LinkContextProvider>
                 <Navigation />
             </LinkContextProvider>
             <Outlet />
-        </div>
+        </>
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import "./_index.scss";
 
 import { store } from "./app/store";
 
