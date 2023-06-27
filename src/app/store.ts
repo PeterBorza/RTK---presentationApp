1846c73f28d82f8393bd762454d07f41e1f1bdb2
Peter Borza <68942716+PeterBorza@users.noreply.github.com>
2023-06-27 17:39:54 +0300

    212 - Hide devTools in production (#212)

--- src/app/store.ts
+++ src/app/store.ts
@@ -7,6 +7,7 @@ export const store = configureStore({
         getDefaultMiddleware({
             immutableCheck: false,
         }),
+    devTools: process.env.NODE_ENV !== "production",
 });
 
 export type RootState = ReturnType<typeof store.getState>;
