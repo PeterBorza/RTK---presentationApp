3a70fafa7cad4f027a5ac0966ecb6f2225da49ff
Peter Borza <68942716+PeterBorza@users.noreply.github.com>
2024-06-12 08:07:08 +0300

    0229 - index file - createRoot imported from react-dom/client (#231)

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,13 @@ import ReactDOM from "react-dom";
 import AppRoutes from "app";
 import Providers from "providers/providers";
 
+const rootContainer = document.getElementById("root");
+
 ReactDOM.render(
     <React.StrictMode>
         <Providers>
             <AppRoutes />
-        </Providers>,
+        </Providers>
     </React.StrictMode>,
-    document.getElementById("root"),
+    rootContainer,
 );
